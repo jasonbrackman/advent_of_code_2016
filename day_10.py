51fd1dd4b6db3ab850f45d79e4d77cd033daf6bc
jasonbrackman <brackman@gmail.com>
2019-11-03 16:45:38 -0800

    part2 was already done accidentally when implementing part01

--- day_10.py
+++ day_10.py
@@ -1,5 +1,6 @@
 import helpers
 
+
 class Network:
     output = dict()
     bots = dict()
@@ -46,12 +47,7 @@ class Network:
                 else:
                     self.bots[bot_id]["values"].append(src_value)
 
-        for line in lines:
-            src_type, src_value, cmd, *args = line.split()
-            src_value = int(src_value)
-
             if cmd == "gives":
-
                 low_name, low_value = args[2], int(args[3])
                 max_name, max_value = args[7], int(args[8])
                 if src_value not in self.bots:
@@ -70,8 +66,17 @@ class Network:
                 return k
         return None
 
+    def get_part_02(self):
+        a = self.output[0]
+        b = self.output[1]
+        c = self.output[2]
+        return a * b * c
+
 
 if __name__ == "__main__":
     n = Network()
     part_01 = n.get_part_01()
     assert part_01 == 113
+
+    part_02 = n.get_part_02()
+    assert part_02 == 12803
