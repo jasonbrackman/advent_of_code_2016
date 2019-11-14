0b45fdc2c2c923a9f07b9c5c3a965a7802ccbbdf
jasonbrackman <brackman@gmail.com>
2019-11-13 23:40:48 -0800

    code cleanup

--- day_14.py
+++ day_14.py
@@ -1,11 +1,14 @@
-from hashlib import md5
 import re
-
-salt = "yjdafjpo"
-# salt = "abc"
+from hashlib import md5
 
 needle3 = re.compile(r"(.)\1{2}")
 needle5 = re.compile(r"(.)\1{4}")
+
+
+def stretch(start):
+    for x in range(2016):
+        start = md5(start.encode()).hexdigest()
+    return start
 
 
 def yield_number(start: int):
@@ -14,10 +17,12 @@ def yield_number(start: int):
         start += 1
 
 
-def yield_hash():
+def yield_hash(salt, stretched=False):
     for index in yield_number(0):
         key = f"{salt}{index}".encode()
         md5_hash = md5(key).hexdigest()
+        if stretched:
+            md5_hash = stretch(md5_hash)
 
         # need to check length of five first
         result = re.findall(needle5, md5_hash)
@@ -32,8 +37,8 @@ def yield_hash():
             yield (index, result[0] * 3, md5_hash)
 
 
-if __name__ == "__main__":
-    hashes = yield_hash()
+def find_keys(salt, stretched):
+    hashes = yield_hash(salt, stretched=stretched)
     found = []
     visited = []
     while len(found) < 65:
@@ -41,14 +46,24 @@ if __name__ == "__main__":
         if len(key) == 5:
             for v in sorted(visited, key=lambda x: x[0]):
                 if i - 1000 < v[0] < i and v[1] == key[0] * 3:
-                    # print(v[0], i-1000, i)
                     if v not in found:
                         found.append(v)
         else:
             visited.append((i, key))
 
+    return found
+
 
 if __name__ == "__main__":
+    salt = "yjdafjpo"
+    # salt = "abc"
+
+    found = find_keys(salt, stretched=False)
     for i, v in enumerate(sorted(set(found), key=lambda x: x[0]), 1):
         if i == 64:
-            print(i, v)
+            print("Part01 =", i, v)
+
+    found = find_keys(salt, stretched=True)
+    for i, v in enumerate(sorted(set(found), key=lambda x: x[0]), 1):
+        if i == 64:
+            print("Part02 =", i, v)
