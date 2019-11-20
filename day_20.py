ff3dd2b512753be4b39145b0945713d7ce7ae88d
jasonbrackman <brackman@gmail.com>
2019-11-20 23:31:20 +0000

    minor cleanup

--- day_20.py
+++ day_20.py
@@ -1,10 +1,12 @@
 import helpers
 
-last_i = -1
 max = 4294967295
-lines = helpers.get_lines(r"./data/day_20.txt")
 
+last_i = -1
 last_o = -1
+
+# Parse the data
+lines = helpers.get_lines(r"./data/day_20.txt")
 
 stuff = list()
 for line in lines:
@@ -12,12 +14,9 @@ for line in lines:
     i, o = int(i), int(o)
     stuff.append((i, o))
 
-for line in sorted(stuff):
-    print(line)
-
+# Collect Gaps
 allowed = list()
 for line in sorted(stuff):
-    # print(line)
     i, o = line
 
     if last_o >= i or i == last_o + 1:
@@ -31,4 +30,7 @@ for line in sorted(stuff):
         last_o = o
 
 print("Part_01:", allowed[0])
-print("Part_02:", len(allowed))
+
+# max might not have been hit so check it.
+part_02 = len(allowed) + max - last_o
+print("Part_02:", part_02)
